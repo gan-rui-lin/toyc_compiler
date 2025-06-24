38e5a1dfac89410d18d04acfdda8633d2afd8047
doodah30 <1322339684@qq.com>
2025-06-24 20:01:06 +0800

    实现了break和continue，解决了return后续的问题，还添加了&&和||的短路逻辑，整体无恙但仍存在一些漏洞

--- lib/astToIR.ml
+++ lib/astToIR.ml
@@ -1,3 +1,4 @@
+(*astToIR.ml*)
 (* 引入 AST 和 IR 类型 *)
 open Ast
 open Ir
@@ -5,9 +6,11 @@ open Ir
 (* Map<String, operand> *)
 module Env = Map.Make(String)
 
-(* 
-  TODO: function(参数传递策略), break, continue, return
-*)
+type context = {
+  env: operand Env.t;
+  break_lbl: string option;     (* break 跳转目标 *)
+  continue_lbl: string option;  (* continue 跳转目标 *)
+}
 
 (* 临时寄存器生成器 *)
 let temp_id = ref 0
@@ -33,108 +36,187 @@ let string_of_binop = function
 let string_of_unop = function
   | Not -> "!" | Plus -> "+" | Minus -> "-"
 
+(* stmt_to_res 用于处理 return 终止：Normal/Returned 两种结果 *)
+type stmt_res =
+  | Normal of ir_inst list
+  | Returned of ir_inst list
+
+(* 将 stmt_res 展平为代码列表 *)
+let flatten = function
+  | Normal code | Returned code -> code
+
 (* 表达式转换：返回目标寄存器和 IR 指令列表 *)
-let rec expr_to_ir (env : operand Env.t) (e : expr) : operand * ir_inst list =
+let rec expr_to_ir (ctx: context) (e: expr) : operand * ir_inst list =
   match e with
   | Number n -> Imm n, []
   | ID name ->
-      let operand = Env.find name env in
+      let operand = Env.find name ctx.env in
       operand, []
-  | Binop (op, e1, e2) ->
-      let lhs, code1 = expr_to_ir env e1 in
-      let rhs, code2 = expr_to_ir env e2 in
-      let res = fresh_temp () in
-      res, code1 @ code2 @ [Binop (string_of_binop op, res, lhs, rhs)]
   | Unop (op, e1) ->
-      let operand, code = expr_to_ir env e1 in
+      let operand, code = expr_to_ir ctx e1 in
       let res = fresh_temp () in
       res, code @ [Unop (string_of_unop op, res, operand)]
-  | Call (fname, args) ->
-      let args_code, arg_operands =
-        List.fold_right (fun arg (code_acc, op_acc) ->
-          let op, code = expr_to_ir env arg in
-          code @ code_acc, op :: op_acc
-        ) args ([], [])
+
+  | Binop (Land, e1, e2) ->
+      (* 短路与: a && b *)
+      let lhs, c1 = expr_to_ir ctx e1 in
+      let res = fresh_temp () in
+      let l_false = fresh_label () in
+      let l_end   = fresh_label () in
+      (* 1. 如果 lhs==0 跳到 false *)
+      let code =
+        c1
+        @ [IfGoto (lhs, l_false)]
+        (* 2. 否则计算 rhs *)
+        @ (let rhs, c2 = expr_to_ir ctx e2 in c2 @ [Assign (res, rhs)])
+        (* 3. 跳到结束 *)
+        @ [Goto l_end; Label l_false; Assign (res, Imm 0); Label l_end]
+      in
+      res, code
+
+  | Binop (Lor, e1, e2) ->
+      (* 短路或: a || b *)
+      let lhs, c1 = expr_to_ir ctx e1 in
+      let res = fresh_temp () in
+      let l_true  = fresh_label () in
+      let l_end   = fresh_label () in
+      let code =
+        c1
+        (* 如果 lhs != 0 跳到 true *)
+        @ [IfGoto (lhs, l_true)]
+        (* 否则计算 rhs *)
+        @ (let rhs, c2 = expr_to_ir ctx e2 in c2 @ [Assign (res, rhs)])
+        @ [Goto l_end; Label l_true; Assign (res, Imm 1); Label l_end]
+      in
+      res, code
+
+  | Binop (op, e1, e2) ->
+      (* 普通二元运算 *)
+      let lhs, c1 = expr_to_ir ctx e1 in
+      let rhs, c2 = expr_to_ir ctx e2 in
+      let dst = fresh_temp () in
+      dst, c1 @ c2 @ [Binop (string_of_binop op, dst, lhs, rhs)]
+
+  | Call (f, args) ->
+      (* 参数顺序按出现顺序计算 *)
+      let codes, ops =
+        List.fold_left (fun (acc_c, acc_o) arg ->
+          let o, c = expr_to_ir ctx arg in
+          (acc_c @ c, acc_o @ [o])
+        ) ([], []) args
       in
       let ret = fresh_temp () in
-      ret, args_code @ [Call (ret, fname, arg_operands)]
+      ret, codes @ [Call (ret, f, ops)]
 
-(* 语句转换：返回 IR 指令列表和更新后的环境 *)
-let rec stmt_to_ir (env : operand Env.t) (s : stmt) : ir_inst list * operand Env.t =
+(* 语句翻译，返回 Normal/Returned，支持块作用域、break/continue、return 提前终止 *)
+let rec stmt_to_res (ctx: context) (s: stmt) : stmt_res =
   match s with
-  | Empty -> [], env
+  | Empty -> Normal []
   | ExprStmt e ->
-      let _, code = expr_to_ir env e in
-      code, env
-  (* 赋初值 *)
-  | Decl (name, Some e) ->
-      let op, code = expr_to_ir env e in
-      let var = Var name in
-      code @ [Assign (var, op)], Env.add name var env
-  | Decl (name, None) ->
-      let var = Var name in
-      [], Env.add name var env
-  | Assign (name, e) ->
-      let op, code = expr_to_ir env e in
-      let var = Env.find name env in
-      code @ [Assign (var, op)], env
-  | Return None -> [Ret None], env
-  | Return Some e ->
-      let op, code = expr_to_ir env e in
-      code @ [Ret (Some op)], env
-  | If (cond, then_stmt, else_stmt) ->
-      let cond_op, cond_code = expr_to_ir env cond in
-      let then_code, _ = stmt_to_ir env then_stmt in
-      let else_code, _ = match else_stmt with
-        | Some s -> stmt_to_ir env s
-        | None -> [], env
-      in
-      let l_then = fresh_label () in
-      let l_else = fresh_label () in
-      let l_end = fresh_label () in
+      let _, code = expr_to_ir ctx e in Normal code
+
+  | Decl (_x, None) ->
+      (* 无初始化的声明仅更新 env *)
+      Normal []
+  | Decl (x, Some e) ->
+      let v, c = expr_to_ir ctx e in
+      let var = Var x in
+      Normal (c @ [Assign (var, v)])
+
+  | Assign (x, e) ->
+      let v, c = expr_to_ir ctx e in
+      let var = Env.find x ctx.env in
+      Normal (c @ [Assign (var, v)])
+
+  | Return None -> Returned [Ret None]
+  | Return (Some e) ->
+      let v, c = expr_to_ir ctx e in
+      Returned (c @ [Ret (Some v)])
+
+  | If (cond, tstmt, Some fstmt) ->
+      let cnd, ccode = expr_to_ir ctx cond in
+      let lthen = fresh_label () and lelse = fresh_label () and lend = fresh_label () in
+      let then_res = stmt_to_res ctx tstmt in
+      let else_res = stmt_to_res ctx fstmt in
+      let then_code = flatten then_res in
+      let else_code = flatten else_res in
+      (* 合并：短路结构中 return 保持位置 *)
       let code =
-        cond_code @
-        [IfGoto (cond_op, l_then); Goto l_else; Label l_then] @
-        then_code @
-        [Goto l_end; Label l_else] @
-        else_code @
-        [Label l_end]
+        ccode
+        @ [IfGoto (cnd, lthen); Goto lelse]
+        @ [Label lthen] @ then_code @ [Goto lend]
+        @ [Label lelse] @ else_code @ [Label lend]
       in
-      code, env
-  | While (cond, body_stmt) ->
-      let l_cond = fresh_label () in
-      let l_body = fresh_label () in
-      let l_end = fresh_label () in
-      let cond_op, cond_code = expr_to_ir env cond in
-      let body_code, _ = stmt_to_ir env body_stmt in
+      (match then_res, else_res with
+       | Returned _, _ | _, Returned _ -> Returned code
+       | _ -> Normal code)
+
+  | If (cond, tstmt, None) ->
+      let cnd, ccode = expr_to_ir ctx cond in
+      let lthen = fresh_label () in
+      let then_res = stmt_to_res ctx tstmt in
+      let then_code = flatten then_res in
+      let code = ccode @ [IfGoto (cnd, lthen)] @ [Label lthen] @ then_code in
+      (match then_res with Returned _ -> Returned code | _ -> Normal code)
+
+  | While (cond, body) ->
+      (* 循环标签 *)
+      let lcond = fresh_label () and lbody = fresh_label () and lend = fresh_label () in
+      let ctx_loop = { ctx with break_lbl = Some lend; continue_lbl = Some lcond } in
+      let cnd, ccode = expr_to_ir ctx_loop cond in
+      let body_res = stmt_to_res ctx_loop body in
+      let bcode = flatten body_res in
       let code =
-        [Goto l_cond; Label l_cond] @ cond_code @ [IfGoto (cond_op, l_body); Goto l_end; Label l_body] @
-        body_code @ [Goto l_cond; Label l_end]
+        [Goto lcond; Label lcond]
+        @ ccode @ [IfGoto (cnd, lbody); Goto lend]
+        @ [Label lbody] @ bcode @ [Goto lcond; Label lend]
       in
-      code, env
+      (* 无法从循环体中直接 return：若想支持可在 body_res 捕获 *)
+      Normal code
+
+  | Break -> (
+      match ctx.break_lbl with
+      | Some lbl -> Normal [Goto lbl]
+      | None -> failwith "break used outside loop"
+    )
+  | Continue -> (
+      match ctx.continue_lbl with
+      | Some lbl -> Normal [Goto lbl]
+      | None -> failwith "continue used outside loop"
+    )
+
   | Block stmts ->
-      (* TODO: 识别到 return 应该退出而非继续推导 *)
-      List.fold_left (fun (acc_code, acc_env) stmt ->
-        let code, new_env = stmt_to_ir acc_env stmt in
-        acc_code @ code, new_env
-      ) ([], env) stmts
-  | Break | Continue ->
-      (* TODO: 不好实现 *)
-      failwith "Break/Continue not implemented yet"
+      (* 块作用域隔离 *)
+      let saved_env = ctx.env in
+      let rec loop env_acc acc_code = function
+        | [] -> Normal acc_code
+        | hd::tl ->
+            let ctx' = { ctx with env = env_acc } in
+            let res = stmt_to_res ctx' hd in
+            let code_hd = flatten res in
+            let env_next = match hd with
+              | Decl (x, _) -> Env.add x (Var x) env_acc
+              | _ -> env_acc
+            in
+            (match res with
+             | Normal _ -> loop env_next (acc_code @ code_hd) tl
+             | Returned _ -> Returned (acc_code @ code_hd))
+      in
+      (* 执行块体 *)
+      let res = loop saved_env [] stmts in
+      (* 恢复外层 env *)
+      (* ctx.env <- saved_env *)
+      res
 
 (* 函数转换 *)
-let func_to_ir (f : func_def) : ir_func =
-  (* 函数参数作为初始的 env *)
-  (* add <name, Var name> *)
-  let env = List.fold_left (fun acc name -> Env.add name (Var name) acc) Env.empty f.params in
-  (* 将 f.body 解析为 Block 并扔给 stmt *)
-  let code, _ = stmt_to_ir env (Block f.body) in
-  {
-    name = f.func_name;
-    args = f.params;
-    body = code;
-  }
-
+let func_to_ir (f: func_def) : ir_func =
+  (* 初始化 env: 参数映射 *)
+  let init_env = List.fold_left (fun m x -> Env.add x (Var x) m) Env.empty f.params in
+  let ctx0 = { env = init_env; break_lbl = None; continue_lbl = None } in
+  (* 翻译函数体 *)
+  let body_res = stmt_to_res ctx0 (Block f.body) in
+  let body_code = flatten body_res in
+  { name = f.func_name; args = f.params; body = body_code }
 (* 编译单元转换 *)
 let program_to_ir (cu : comp_unit) : ir_program =
   List.map func_to_ir cu
