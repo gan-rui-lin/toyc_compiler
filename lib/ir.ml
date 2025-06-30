b13b6e54efee11f0a2298650449b2acada2820c5
grl <857308047@qq.com>
2025-06-30 16:28:54 +0800

    IR Block 定义

--- lib/ir.ml
+++ lib/ir.ml
@@ -8,12 +8,32 @@ type ir_inst =
   | Unop of string * operand * operand (* t1 = -t2 *)
   | Load of operand * operand (* t1 = *t2 *)
   | Store of operand * operand (* *t1 = t2 *)
-  | Goto of string
-  | IfGoto of operand * string
-  | Label of string
-  | Call of operand * string * operand list (* t1 = call f(args) *)
-  | Ret of operand option
+  | Goto of string (* 优化的 IR 没有 *)
+  | IfGoto of operand * string (* 优化的 IR 没有 *)
+  | Label of string (* 优化的 IR 没有 *)
+  | Call of operand * string * operand list
+  (* 优化后的 IR 没有 *)
+  (* t1 = call f(args) *)
+  | Ret of operand option (* 优化后的 IR 没有 *)
   | Assign of operand * operand (* t1 = t2 *)
 
 type ir_func = { name : string; args : string list; body : ir_inst list }
-type ir_program = ir_func list
+
+(* 用于优化 *)
+type ir_term =
+  | TermGoto of string
+  | TermIf of operand * string * string
+  | TermRet of operand option
+  | TermUnreachable
+
+type ir_block = {
+  label : string;
+  insts : ir_inst list;
+  terminator : ir_term;
+  mutable preds : string list;
+  mutable succs : string list;
+}
+
+(* 用于 IR 优化的 func 定义 *)
+type ir_func_o = { name : string; args : string list; blocks : ir_block list }
+type ir_program = Ir_funcs of ir_func list | Ir_funcs_o of ir_func_o list
---
+++ test/1.s
@@ -0,0 +1,142 @@
+.text
+.global main
+
+f:
+	addi sp, sp, -256
+	sw a0, -4(sp)
+	lw t1, -4(sp)
+	li t2, 3
+	rem t0, t1, t2
+	sw t0, -8(sp)
+	lw a0, -8(sp)
+	addi sp, sp, 256
+	ret
+
+main:
+	addi sp, sp, -256
+	li t0, 0
+	sw t0, -4(sp)
+	li t0, 0
+	sw t0, -8(sp)
+	lw t1, -4(sp)
+	li t2, 1
+	sub t0, t1, t2
+	sw t0, -12(sp)
+	lw t0, -12(sp)
+	sw t0, -16(sp)
+	j L0
+L0:
+	lw t1, -4(sp)
+	li t2, 5
+	slt t0, t1, t2
+	sw t0, -20(sp)
+	lw t0, -20(sp)
+	bne t0, x0, L1
+	j L2
+L1:
+	lw t1, -4(sp)
+	li t2, 1
+	sub t0, t1, t2
+	sw t0, -24(sp)
+	lw t0, -24(sp)
+	sw t0, -4(sp)
+	lw t1, -4(sp)
+	li t2, 1
+	sub t0, t1, t2
+	sw t0, -28(sp)
+	lw a0, -28(sp)
+	call f
+	sw a0, -32(sp)
+	lw t1, -32(sp)
+	li t2, 0
+	sub t0, t1, t2
+	seqz t0, t0
+	sw t0, -36(sp)
+	lw t0, -36(sp)
+	bne t0, x0, L3
+	j L4
+L3:
+	j L0
+L4:
+	li t0, 0
+	sw t0, -8(sp)
+	j L5
+L5:
+	lw t1, -8(sp)
+	li t2, 4
+	slt t0, t1, t2
+	sw t0, -40(sp)
+	lw t0, -40(sp)
+	bne t0, x0, L6
+	j L7
+L6:
+	lw t1, -16(sp)
+	lw t2, -8(sp)
+	add t0, t1, t2
+	sw t0, -44(sp)
+	lw t0, -44(sp)
+	sw t0, -16(sp)
+	lw t1, -16(sp)
+	li t2, 10
+	sgt t0, t1, t2
+	sw t0, -48(sp)
+	lw t0, -48(sp)
+	bne t0, x0, L8
+	j L9
+L8:
+	j L7
+L9:
+	lw t1, -8(sp)
+	li t2, 2
+	rem t0, t1, t2
+	sw t0, -52(sp)
+	lw t1, -52(sp)
+	li t2, 0
+	sub t0, t1, t2
+	seqz t0, t0
+	sw t0, -56(sp)
+	lw t0, -56(sp)
+	bne t0, x0, L10
+	j L11
+L10:
+	lw t1, -8(sp)
+	li t2, 1
+	add t0, t1, t2
+	sw t0, -60(sp)
+	lw t0, -60(sp)
+	sw t0, -8(sp)
+	j L5
+L11:
+	lw a0, -8(sp)
+	call f
+	sw a0, -64(sp)
+	lw t1, -16(sp)
+	lw t2, -64(sp)
+	add t0, t1, t2
+	sw t0, -68(sp)
+	lw t0, -68(sp)
+	sw t0, -16(sp)
+	lw t1, -8(sp)
+	li t2, 1
+	add t0, t1, t2
+	sw t0, -72(sp)
+	lw t0, -72(sp)
+	sw t0, -8(sp)
+	j L5
+L7:
+	lw t1, -16(sp)
+	li t2, 20
+	sgt t0, t1, t2
+	sw t0, -76(sp)
+	lw t0, -76(sp)
+	bne t0, x0, L12
+	j L13
+L12:
+	j L2
+L13:
+	j L0
+L2:
+	lw a0, -16(sp)
+	addi sp, sp, 256
+	ret
+
