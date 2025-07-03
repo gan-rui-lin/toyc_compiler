327cc79ff3d006d29e42315e4083feea9e0a7aa5
grl <857308047@qq.com>
2025-07-03 10:35:41 +0800

    test func

--- test/temp.s
+++ test/temp.s
@@ -1,7 +1,7 @@
 .text
  .global main
 add:
-	addi sp, sp, -64
+	addi sp, sp, -256
 	sw a0, 4(sp)
 	sw a1, 8(sp)
 	lw t1, 4(sp)
@@ -9,11 +9,11 @@ add:
 	add t0, t1, t2
 	sw t0, 12(sp)
 	lw a0, 12(sp)
-	addi sp, sp, 64
+	addi sp, sp, 256
 	ret
 
 main:
-	addi sp, sp, -64
+	addi sp, sp, -256
 	li a0, 116
 	li a1, 105
 	call add
@@ -21,6 +21,6 @@ main:
 	lw t0, 4(sp)
 	sw t0, 8(sp)
 	lw a0, 8(sp)
-	addi sp, sp, 64
+	addi sp, sp, 256
 	ret
 
