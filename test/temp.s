cea192d88adb04263fd58a6d516f7ba3567cc285
grl <857308047@qq.com>
2025-07-03 15:21:24 +0800

    temp.s

--- test/temp.s
+++ test/temp.s
@@ -1,51 +1,54 @@
-# AST:
+AST:
 
-# Function f(n) : int {
-#   Return(Binop(%, ID(n), Number(3)))
-# }
+Function f(n) : int {
+  Return(Binop(%, ID(n), Number(3)))
+}
 
-# Function main() : int {
-#   Decl(x, Call(f, [Number(5)]))
-#   Return(ID(x))
-# }
+Function main() : int {
+  Decl(x, Call(f, [Number(5)]))
+  Return(ID(x))
+}
 
-# IR:
+IR:
 
-# function f(n):
-#   t0 = n % 3
-#   return t0
-# function main():
-#   t1 = call f(5)
-#   x = t1
-#   return x
+function f(n):
+LABEL0:
+    t0 = n % 3
+    return t0
+  terminator: return t0
+    preds: []
+    succs: []
+function main():
+LABEL2:
+    t0 = call f(5)
+    x = t0
+    return x
+  terminator: return x
+    preds: []
+    succs: []
 
 
-# .text
-#  .global main
-
+.text
+ .global main
+f:
+	addi sp, sp, -256
+	sw a0, 4(sp)
+	sw ra, 8(sp)
+	li t1, 3
+	rem t0, a0, t1
+	mv a0, t0
+	lw ra, 8(sp)
+	addi sp, sp, 256
+	ret
 
 main:
 	addi sp, sp, -256
 	sw ra, 4(sp)
 	li a0, 5
 	call f
-	sw a0, 8(sp)
-	lw t0, 8(sp)
-	sw t0, 12(sp)
-	lw a0, 12(sp)
+	mv t1, a0
+	mv a0, t1
 	lw ra, 4(sp)
 	addi sp, sp, 256
 	ret
 
-f:
-	addi sp, sp, -256
-	sw a0, 4(sp)
-	sw ra, 8(sp)
-	lw t1, 4(sp)
-	li t2, 3
-	rem t0, t1, t2
-	sw t0, 12(sp)
-	lw a0, 12(sp)
-	lw ra, 8(sp)
-	addi sp, sp, 256
-	ret
