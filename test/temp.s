f388b945ded9a2961b8a9e3adf733714a4908e06
grl <857308047@qq.com>
2025-07-03 11:16:59 +0800

    test

--- test/temp.s
+++ test/temp.s
@@ -1,26 +1,51 @@
+# AST:
+
+# Function f(n) : int {
+#   Return(Binop(%, ID(n), Number(3)))
+# }
+
+# Function main() : int {
+#   Decl(x, Call(f, [Number(5)]))
+#   Return(ID(x))
+# }
+
+# IR:
+
+# function f(n):
+#   t0 = n % 3
+#   return t0
+# function main():
+#   t1 = call f(5)
+#   x = t1
+#   return x
 
 
-.text
- .global main
-f:
-	addi sp, sp, -256
-	sw a0, 4(sp)
-	lw t1, 4(sp)
-	li t2, 3
-	rem t0, t1, t2
-	sw t0, 8(sp)
-	lw a0, 8(sp)
-	addi sp, sp, 256
-	ret
+# .text
+#  .global main
+
 
 main:
 	addi sp, sp, -256
+	sw ra, 4(sp)
 	li a0, 5
 	call f
-	sw a0, 4(sp)
-	lw t0, 4(sp)
-	sw t0, 8(sp)
-	lw a0, 8(sp)
+	sw a0, 8(sp)
+	lw t0, 8(sp)
+	sw t0, 12(sp)
+	lw a0, 12(sp)
+	lw ra, 4(sp)
 	addi sp, sp, 256
 	ret
 
+f:
+	addi sp, sp, -256
+	sw a0, 4(sp)
+	sw ra, 8(sp)
+	lw t1, 4(sp)
+	li t2, 3
+	rem t0, t1, t2
+	sw t0, 12(sp)
+	lw a0, 12(sp)
+	lw ra, 8(sp)
+	addi sp, sp, 256
+	ret
